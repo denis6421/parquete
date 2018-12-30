29bf5754ce82e934c87e9768f0c1922ff6582eee
denis6421 <denis.b651@gmail.com>
2018-12-31 00:29:53 +0200

    a

--- js/main.js
+++ js/main.js
@@ -40,6 +40,51 @@ $(document).ready(function(){
 
 });
 
+
+$('.category_select').click(function(){
+
+    $('.hidden_category_box').hide();
+    $('#bottom_works_flex').show();
+    $(this).hide()
+    $('#work_gallery_h1').html('פרוייקטים')
+    $('html, body').animate({scrollTop: $('#work_gallery').offset().top -100 }, 3);
+    return false;
+})
+
+
+$('.desktop_slider_btn_bottom').click(function(){
+  $('.category_select').show()
+    $('#bottom_works_flex').hide();
+    $('html, body').animate({scrollTop: $('#work_gallery').offset().top -100 }, 3);
+    return false;
+
+})
+$('.exit_slider_btn').click(function(){
+  $('.sliders').removeClass('show_slider');
+})
+
+$('.desktop_slider_pergula').click(function(){
+  $('.hidden_pergula_box').css('display', 'flex');
+  $('#work_gallery_h1').html('פרגולות')
+})
+
+$('.desktop_slider_deck').click(function(){
+  $('.hidden_deck_box').css('display', 'flex');
+  $('#work_gallery_h1').html('דקים')
+})
+
+
+$('.desktop_slider_stairs').click(function(){
+  $('.hidden_stairs_box').css('display', 'flex');
+  $('#work_gallery_h1').html('מדרגות')
+})
+
+
+$('.desktop_slider_shower').click(function(){
+  $('.hidden_shower_box').css('display', 'flex');
+  $('#work_gallery_h1').html('אמבטיות')
+})
+
 $('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
   $(this).toggleClass('open');
   $('.mobile_menu').toggleClass('opend_menu');
@@ -55,9 +100,7 @@ $('.mobile_menu ul li').click(function(){
 $('.desktop_slider_btn').click(function(){
   $('.hidden_sliders').show()
 })
-$('.mobile_slider_btn').click(function(){
-  $('.hidden_sliders').show()
-})
+
 $('#test_1').click(function(){
   $('.slider_1').addClass('show_slider')
   $('.slider_1').slick('setPosition');
@@ -70,15 +113,15 @@ $('#test_3').click(function(){
   $('.slider_3').addClass('show_slider')
   $('.slider_3').slick('setPosition');
 })
-$('#test_4').click(function(){
+$('#stairs_slider_1').click(function(){
   $('.slider_4').addClass('show_slider')
   $('.slider_4').slick('setPosition');
 })
-$('#test_5').click(function(){
+$('#deck_slider_3').click(function(){
   $('.slider_5').addClass('show_slider')
   $('.slider_5').slick('setPosition');
 })
-$('#test_6').click(function(){
+$('#shower_slider_1').click(function(){
   $('.slider_6').addClass('show_slider')
   $('.slider_6').slick('setPosition');
 })
